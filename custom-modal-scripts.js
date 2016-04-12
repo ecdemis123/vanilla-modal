9550da8fa5aaf86121e1d18a4bcfae33f509440c
Matthew <marplemr@gmail.com>
2016-04-12 01:45:37 -0700

    [bug] fade-out fix bc two transition were firing at once

--- custom-modal-scripts.js
+++ custom-modal-scripts.js
@@ -1,27 +1,23 @@
-var modal = document.getElementById("awesomeModal");
-var placeholderText = document.getElementById("placeholderText");
 
 var alertModal  = function(siteName, messageBody) {
-  // modal.classList = "modal";
-  // console.log(modal.classList);
-  if(modal.classList.contains("fade-out")) {
+  var modal = document.getElementById("awesomeModal");
+  var placeholderText = document.getElementById("placeholderText");
+  if (modal.classList.contains("fade-out")) {
     modal.classList.remove("fade-out");
   }
-
-  if(modal.classList.contains("slide-in")) {
-    modal.classList.remove("slide-in");
-  }
-  if(!messageBody){
+  if (!messageBody){
     messageBody = 'No message set!';
   }
-  // document.querySelector('.modal-content p').innerHTML = siteName + " says " + messageBody;
+  if (!siteName){
+    messageBody = 'No site-name set!';
+  }
   placeholderText.innerHTML = siteName + " says " + messageBody;
   modal.classList.add("slide-in");
-  // console.log(modal.classList);
 }
 
-var closeModal = function() {
-  modal.classList.remove("slide-in");
-  modal.className += " fade-out";
-  // console.log(modal.classList);
+var closeModal = function () {
+  modal.classList.add("fade-out");
+  setTimeout(function () {
+    modal.classList.remove("slide-in");
+  }, 500);
 }
