f084bec727d002c61d07501be7a6c1086b0c6f8d
Eleanor Demis <eleanordemis@gmail.com>
2016-04-11 21:48:17 -0700

    attempting to vertically align modal

--- custom-modal-scripts.js
+++ custom-modal-scripts.js
@@ -1,17 +1,15 @@
 (function() {
 
   var modal = document.getElementById("awesomeModal");
-  var button = document.getElementById("openModalButton");
-
 
   window.alert = function(siteName, messageBody) {
     customAlertModal(siteName, messageBody)
   }
 
   customAlertModal = function(siteName, messageBody) {
-    modal.className = "modal";
     document.querySelector('.modal-content p').innerHTML = siteName + " says " + messageBody;
-    modal.style.display = "block";
+    modal.className = "modal slide-in";
+    // modal.style.display = "block";
   }
 
   closeModal = function() {
