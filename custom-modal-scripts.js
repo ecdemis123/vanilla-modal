580e9294ced499178fbbc0a8e603914cadf31490
Matthew <marplemr@gmail.com>
2016-04-12 00:42:59 -0700

    [feature] a lot of stuff

--- custom-modal-scripts.js
+++ custom-modal-scripts.js
@@ -1,26 +1,27 @@
-(function() {
+var modal = document.getElementById("awesomeModal");
+var placeholderText = document.getElementById("placeholderText");
 
-  var modal = document.getElementById("awesomeModal");
-
-  window.alert = function(siteName, messageBody) {
-    customAlertModal(siteName, messageBody)
+var alertModal  = function(siteName, messageBody) {
+  // modal.classList = "modal";
+  // console.log(modal.classList);
+  if(modal.classList.contains("fade-out")) {
+    modal.classList.remove("fade-out");
   }
 
-  customAlertModal = function(siteName, messageBody) {
-    modal.classList = "modal";
-    console.log(modal.classList);
-    if(modal.classList.contains("slide-in")) {
-      modal.classList.remove("slide-in");
-    }
-    document.querySelector('.modal-content p').innerHTML = siteName + " says " + messageBody;
-    modal.classList.add("slide-in");
-    console.log(modal.classList);
-  }
-
-  closeModal = function() {
+  if(modal.classList.contains("slide-in")) {
     modal.classList.remove("slide-in");
-    modal.className += " fade-out";
-    console.log(modal.classList);
   }
+  if(!messageBody){
+    messageBody = 'No message set!';
+  }
+  // document.querySelector('.modal-content p').innerHTML = siteName + " says " + messageBody;
+  placeholderText.innerHTML = siteName + " says " + messageBody;
+  modal.classList.add("slide-in");
+  // console.log(modal.classList);
+}
 
-})();
+var closeModal = function() {
+  modal.classList.remove("slide-in");
+  modal.className += " fade-out";
+  // console.log(modal.classList);
+}
